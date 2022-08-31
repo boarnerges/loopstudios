cfd2a96ec076d175368e2cd17708568bb7f552a2
olusegun Owolabi <olujawo1996@gmail.com>
2022-08-31 23:23:42 +0100

    desktop view

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 const hamburgerOpen = document.querySelector(".hamburger__open");
 const hamburgerClose = document.querySelector(".hamburger__close");
-const navLink = document.querySelector(".navlink")
+const navLink = document.querySelector(".navlink");
+const title = document.querySelector(".hero-title");
 
 hamburger.addEventListener("click", () => {
  hamburgerClose.classList.toggle("active");
  hamburgerOpen.classList.toggle("none");
  mobileMenu.classList.toggle("active");
  navLink.classList.toggle("active");
+ title.classList.toggle("none");
  
 });
 
 
-// hamburgerClose.addEventListener("click", () => {
-//     // hamburgerClose.classList.toggle("none");
-//     hamburgerOpen.classList.toggle("active");
-//     mobileMenu.classList.toggle("none");
-//     navLink.classList.toggle("none");
-    
-//    });
